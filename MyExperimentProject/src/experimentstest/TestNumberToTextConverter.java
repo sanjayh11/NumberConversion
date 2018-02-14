c46caeb8c11af48c98cff5e7af44a6cf758c4abc
sanjayh11 <35250547+sanjayh11@users.noreply.github.com>
2018-02-14 15:17:34 +0530

    checking in the JUnit test class

--- MyExperimentProject/src/experimentstest/TestNumberToTextConverter.java
+++ MyExperimentProject/src/experimentstest/TestNumberToTextConverter.java
@@ -2,6 +2,7 @@
  * 
  */
 package experimentstest;
+//import javax.swing.JOptionPane;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +11,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Properties;
+
+import javax.swing.JOptionPane;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -40,7 +43,7 @@ class TestNumberToTextConverter {
 		try {
 			Properties prop = new Properties();
 		       
-    		File file = new File ("C:\\Users\\sanja\\git\\NumberConversionProject\\MyExperimentProject\\NumberToTextMapping.properties");
+    	   File file = new File ("C:\\Users\\sanja\\git\\NumberConversionProject\\MyExperimentProject\\NumberToTextMapping.properties");
 			FileInputStream stream = new FileInputStream(file);
 	
 
@@ -53,21 +56,28 @@ class TestNumberToTextConverter {
 				 counter+= 1;
 				String key = (String) enum1.nextElement();
 				String value = prop.getProperty(key);
-				
-			String[] result = new String[1];
-			result[0]="";
-			int num = Integer.parseInt(key);
-			String[] results = nttc.ConvertNumberToText(num, result);
+				String[] result = new String[1];
+			    result[0]="";
+			    int num = Integer.parseInt(key);
+			    String[] results = nttc.ConvertNumberToText(num, result);
 			
-			//System.out.println ("Converter value:" + results[0] +"  Test Value: " + value);
-			assertEquals(results[0].trim(),value.trim());
-			
+			    System.out.println ("Converter value:" + results[0] +"  Test Value: " + value);
+			    assertEquals(results[0].trim(),value.trim());
+			    //assertTrue(true);
+		
+			    // String test1;
+			    //  test1= JOptionPane.showInputDialog("Please input a number (between 1 and 100,000 for conversion to text: ");
+
+			    // int int1 = Integer.parseInt(test1);
+			    //System.out.println("Inpt integer is:"+ int1);
+		
+			    //assertTrue(true);
 					
-}
+			}//end while
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+}
 
 }
